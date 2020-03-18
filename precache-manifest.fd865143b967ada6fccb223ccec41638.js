5bedabca004253c5b7d8016a6c1940ec35133ede
ramezyousri21 <53097570+ramezyousri21@users.noreply.github.com>
2020-03-18 10:03:01 +0200

    Updates

---
+++ precache-manifest.fd865143b967ada6fccb223ccec41638.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4875f4753b87221480ef6d3a162fa3ce",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "dd62eb06ae7394f8bfa7",
+    "url": "/robofriends/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7ca8e3cd38b62aabcc91",
+    "url": "/robofriends/static/js/2.17d5f4f8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/robofriends/static/js/2.17d5f4f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd62eb06ae7394f8bfa7",
+    "url": "/robofriends/static/js/main.d501128b.chunk.js"
+  },
+  {
+    "revision": "46690a7b9ddc777b3b4c",
+    "url": "/robofriends/static/js/runtime-main.ba4515c0.js"
+  }
+]);
